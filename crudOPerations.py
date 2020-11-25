59e5c5aa59c3b3070985ac41605568d01fc59806
marutibhosale <59178777+marutibhosale@users.noreply.github.com>
2020-11-25 15:05:43 +0530

    Add: update method

--- crudOPerations.py
+++ crudOPerations.py
@@ -1,13 +1,14 @@
 from flask import Flask, request, render_template, redirect, url_for
 from flask_mysqldb import MySQL
-import os
+
+from jinja2 import UndefinedError
 
 
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = os.environ['user_name']
-app.config['MYSQL_PASSWORD'] = os.environ['password']
+app.config['MYSQL_USER'] = '{{ $user_name }}'
+app.config['MYSQL_PASSWORD'] = '{{ $password }}'
 app.config['MYSQL_DB'] = 'flaskCrud'
 
 mysql = MySQL(app)
@@ -38,6 +39,28 @@ def show():
     data = cur.fetchall()
     cur.close()
     return render_template("show.html", data=data)
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    if request.method == "POST":
+        if request.form['update']:
+            name = request.form['name']
+            email = request.form['email']
+
+            cur = mysql.connection.cursor()
+            cur.execute("""UPDATE user SET name=%s, email=%s WHERE id=%s""", (name, email, id))
+            mysql.connection.commit()
+            cur.close()
+            return redirect(url_for("show"))
+
+    try:
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM user WHERE id=%s", (id,))
+        data = cur.fetchall()
+        cur.close()
+        return render_template("update.html", data=data)
+    except UndefinedError:
+        return "Sorry! id " + str(id) + " not exist"
 
 
 if __name__ == "__main__":
